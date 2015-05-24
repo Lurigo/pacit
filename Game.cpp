590cdfa24c32791d8753c884aa60b603ab9e07e6
Lurigo <marduleroux@gmail.com>
2015-05-24 22:25:00 +0200

    Moved some Player related items from Game to Player. Added comments to code. Added direction variable diagram for easy reference.

--- Game.cpp
+++ Game.cpp
@@ -2,7 +2,7 @@
 
 Game::Game(QWidget *parent)
 {
-    // create scene
+    // create and set up scene
     QGraphicsScene *scene = new QGraphicsScene();
     scene->setSceneRect(0,0,800,600);
 
@@ -13,10 +13,6 @@ Game::Game(QWidget *parent)
 
     // create player
     player = new Player();
-    player->setRect(0,0,100,100);
-    player->setPos(400,300);
-    player->setFlag(QGraphicsItem::ItemIsFocusable);
-    player->setFocus();
     scene->addItem(player);
 
     show();
--- Player.cpp
+++ Player.cpp
@@ -2,32 +2,47 @@
 #include <QKeyEvent>
 #include <QTimer>
 
+/* Direction values:
+ * -----------------
+ *        ^ 1
+ *        |
+ *    4 <- -> 2
+ *        |
+ *        v 4
+ \ * * * * * * * * */
+
 Player::Player()
 {
-    dir = 2;
-    QTimer *timer = new QTimer();
+    dir = 0; // Initialise direction indicator
+
+    setRect(0,0,50,50); // Set player object size
+    setPos(400,300); // Set player start position
+    setFlag(QGraphicsItem::ItemIsFocusable); // Make player focusable, for key events
+    setFocus();
+
+    QTimer *timer = new QTimer(); // Create and set up QTimer for momvement
     connect(timer,SIGNAL(timeout()),this,SLOT(move()));
 
-    timer->start(50);
+    timer->start(25); // Set timer interval and start
 }
 
-void Player::keyPressEvent(QKeyEvent *event)
+void Player::keyPressEvent(QKeyEvent *event) // Checks for pressed keys
 {
-    if (event->key() == Qt::Key_Left)
+    if (event->key() == Qt::Key_Up)
     {
-        dir = 4;
+        dir = 1;
     }
     else if (event->key() == Qt::Key_Right)
     {
         dir = 2;
     }
-    else if (event->key() == Qt::Key_Up)
-    {
-        dir = 1;
-    }
     else if (event->key() == Qt::Key_Down)
     {
         dir = 3;
+    }
+    else if (event->key() == Qt::Key_Left)
+    {
+        dir = 4;
     }
 }
 
@@ -36,22 +51,22 @@ void Player::move()
     if (dir == 1) // Up
     {
         if (pos().y() > 0)
-            setPos(x(),y()-10);
+            setPos(x(),y()-5);
     }
     else if (dir == 2) // Right
     {
-        if (pos().x() < 700)
-            setPos(x()+10,y());
+        if (pos().x() < 750)
+            setPos(x()+5,y());
     }
     else if (dir == 3) // Down
     {
-        if (pos().y() < 500)
-            setPos(x(),y()+10);
+        if (pos().y() < 550)
+            setPos(x(),y()+5);
     }
     else if (dir == 4) // Left
     {
         if (pos().x() > 0)
-            setPos(x()-10,y());
+            setPos(x()-5,y());
     }
     else setPos(x(),y());
 }
