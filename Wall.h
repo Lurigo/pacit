c1b65fddffd9f2fcf9d1f5b7db16251d7f14f92c
Lurigo <marduleroux@gmail.com>
2015-05-30 01:57:08 +0200

    Removed Wall collision detection
    Collision detection will be internally handled by the Player, Bullet and
    to-be-added Enemy objects. This is to increase efficiency and
    performance.

--- Wall.h
+++ Wall.h
@@ -1,16 +1,13 @@
 #ifndef WALL_H
 #define WALL_H
 
-#include <QObject>
 #include <QGraphicsItem>
 #include <QGraphicsRectItem>
 
-class Wall: public QObject, public QGraphicsRectItem
-{       Q_OBJECT
+class Wall:public QGraphicsRectItem
+{
 public:
     Wall(int x, int y, QGraphicsItem *parent=0);
-public slots:
-    void detect();
 };
 
 #endif // WALL_H
